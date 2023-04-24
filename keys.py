45c396b117fe97cbb36d25102cfcf5684567f47a
christianpersichetti <123416098+christianpersichetti@users.noreply.github.com>
2023-04-24 17:43:00 -0500

    4/24 Class

---
+++ keys.py
@@ -0,0 +1,16 @@
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
+
+mapquest_key = ""
+
+
+api_key = ""
+api_key_secret = ""
+bearer_token = ""
+
+
+accountSID = "AC14b586e7011e379fd61c6edff3252976"
+
+auth_token = "AC5accea1bc1a2b6c936014b2ca7d0f9cd"
